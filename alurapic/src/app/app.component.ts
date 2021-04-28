3ada2711a939f82f400316cdfdb5b0409dd91e43
Gabriela Vespoli <gabiat3@hotmail.com>
2021-04-27 23:32:05 -0300

    initial commit

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title = "alurapic";
+
+  photos = [
+    {
+      url: "https://source.unsplash.com/random",
+      description: "randomic",
+    },
+    {
+      url: "https://source.unsplash.com/random",
+      description: "random",
+    },
+    {
+      url: "https://source.unsplash.com/random",
+      description: "random",
+    },
+  ];
+}
