2464220d12fc7325be9fa535bbc8b446628ae0eb
Gabriela Vespoli <gabiat3@hotmail.com>
2021-08-08 19:52:18 -0300

    feat: component card and ng-content

--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CardModule } from 'src/app/shared/components/card/card.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -13,6 +14,6 @@ import { PhotosComponent } from './photos/photos.component';
     LoadButtonComponent,
     FilterByDescription,
   ],
-  imports: [CommonModule, PhotoModule],
+  imports: [CommonModule, PhotoModule, CardModule],
 })
 export class PhotoListModule {}
