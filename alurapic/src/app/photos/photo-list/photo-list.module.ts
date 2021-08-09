a1ddcf189b047a9ee48f56dd4517ce0125bd6e62
Gabriela Vespoli <gabiat3@hotmail.com>
2021-08-08 21:06:01 -0300

    feat: finalized implement directive

--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -16,6 +17,6 @@ import { SearchComponent } from './search/search.component';
     FilterByDescription,
     SearchComponent,
   ],
-  imports: [CommonModule, PhotoModule, CardModule],
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 })
 export class PhotoListModule {}
